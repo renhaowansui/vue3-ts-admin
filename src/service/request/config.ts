3bb40a163b3805ed2273323f03fb6e46c0083537
renhao <renhao1@sunline.cn>
2023-05-06 17:16:59 +0800

    feat(打包优化)清除console

--- src/service/request/config.ts
+++ src/service/request/config.ts
@@ -4,9 +4,9 @@ const TIME_OUT = 10000
 if (process.env.NODE_ENV === 'development') {
   BASE_URL = 'http://123.207.32.32:5000'
 } else if (process.env.NODE_ENV === 'production') {
-  BASE_URL = 'http://codercba.com/prod'
+  BASE_URL = 'http://123.207.32.32:5000'
 } else {
-  BASE_URL = 'http://codercba.com/test'
+  BASE_URL = 'http://123.207.32.32:5000'
 }
 
 export { BASE_URL, TIME_OUT }
